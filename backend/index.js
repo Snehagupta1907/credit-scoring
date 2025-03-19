ecde22155c1395141ec5a5214cb11544fff190ca
sneha gupta <snehagupta98930@gmail.com>
2025-03-19 09:46:40 +0530

    deploy

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,6 @@
-import express from "express";
-import helmet from "helmet";
-import cors from "cors";
-import rateLimit from "express-rate-limit";
-import dotenv from "dotenv";
-import Moralis from "moralis";
-import walletRoutes from "./routes/walletRoutes.js";
-import creditScoreRoutes from "./routes/creditScoreRoutes.js";
+import app from "./app.js";
 
-dotenv.config();
-
-const app = express();
 const port = process.env.PORT || 3000;
-
-// Middleware
-app.use(express.json()); // Built-in JSON parser
-app.use(helmet()); // Security headers
-app.use(cors()); // Enable CORS
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-  message: { error: "Too many requests, please try again later." },
-});
-app.use(limiter);
-
-// Initialize Moralis
-await Moralis.start({ apiKey: process.env.MORALIS_API_KEY });
-
-// Use the routes
-app.use("/api", walletRoutes);
-app.use("/api", creditScoreRoutes);
-
 app.listen(port, () => {
   console.log(`Server running on port :${port}`);
 });
