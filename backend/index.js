79ddd7391418bac41d8bf241859bd17b6b8d9709
sneha gupta <snehagupta98930@gmail.com>
2025-03-19 09:51:36 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,20 @@
 import app from "./app.js";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const port = process.env.PORT || 3000;
+
+process.on('uncaughtException', err => {
+  console.error('Uncaught Exception:', err);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+  process.exit(1);
+});
+
 app.listen(port, () => {
   console.log(`Server running on port :${port}`);
 });
